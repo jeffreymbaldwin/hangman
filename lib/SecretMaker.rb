4334cceee8bc9efcd676e5c779c4a2962a5d2fe4
Jeff <jmartinbaldwin@gmail.com>
2026-02-27 10:11:48 +0900

    Implement secret word maker

--- lib/SecretMaker.rb
+++ lib/SecretMaker.rb
@@ -1,32 +1,18 @@
 class SecretMaker
   
   def initialize
-    word_list = File.readlines("words.txt")
-    p word_list
-    # word_list.each do |word|
-    #   word.chomp
-      
+    raw_word_list = File.readlines("words.txt")
     
+    normalized_word_list = raw_word_list.map do |word|
+        word.chomp
+    end 
+    word_bank = normalized_word_list.select do |word|
+      word.length >= 6
+    end
+    @secret_word = word_bank.sample
+    p @secret_word
   end
-
 
 end
 
 
-# Correct mental pipeline (no code)
-
-# You need:
-
-# Read file as lines → get array
-
-# Transform each line → remove newline
-
-# Store the cleaned result somewhere
-
-# Then filter
-
-# Right now you’re at:
-
-# Step 1 is incorrect (you’re getting one big string)
-
-# Step 2 isn’t storing the cleaned result
--- main.rb
+++ main.rb
@@ -1,3 +1,11 @@
 require_relative 'lib/Game'
 
 game = Game.new
+
+
+#we are working on secret maker. 
+#I have it changed to an array
+#now I need to chomp it
+#and get rid of the words i dont want
+#then I will take that result and randomize one word for 
+#the secret
